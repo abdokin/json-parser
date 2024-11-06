f2958bc576f5291d1d1bbb4486477a502e85c892
abdokin <bashbin.sh@gmail.com>
2024-11-06 11:47:39 -0600

    Add JSON parser command-line tool

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,20 @@
-print("Hello, world!")
+import ArgumentParser
+import Foundation
+
+struct JsonParser: ParsableCommand {
+    @Argument(help: "The path to the JSON file to parse.")
+    var jsonFilePath: String
+
+    func run() throws {
+        let fileURL = URL(fileURLWithPath: jsonFilePath)
+        let jsonData = try Data(contentsOf: fileURL)
+        guard let jsonString = String(data: jsonData, encoding: .utf8) else {
+            throw NSError(domain: "JsonParserErrorDomain", code: 1, userInfo: [NSLocalizedDescriptionKey: "Failed to read JSON file"])
+        }
+        var parser = Parser(jsonString)
+        let ast = parser.parse()
+        print(ast)
+    }
+}
+
+JsonParser.main()
